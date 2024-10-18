fd2f3be5248c7640f126a16755d8a9a8f1032a3e
Beethika Saini Thakur <84230125+BeethikaThakur@users.noreply.github.com>
2024-10-18 10:38:19 +0530

    Update Instance.tf
    Tags and Instance type updated

--- Instance.tf
+++ Instance.tf
@@ -1,4 +1,4 @@
-  provider "aws" { 
+ka  provider "aws" { 
   region = "us-east-1" 
   access_key=var.AWS_ACCESS_KEY 
   secret_key=var.AWS_SECRET_KEY 
@@ -6,8 +6,8 @@
 
   resource "aws_instance" "web" { 
   ami           = "ami-0fc5d935ebf8bc3bc" 
-  instance_type = "t3.micro" 
+  instance_type = "t2.micro" 
   tags = { 
-  Name = "Hello from Terraform Cloud" 
+  Name = "Beethika-Hello from Terraform Cloud" 
   } 
   }
